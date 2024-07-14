b868049597e23472c389a0d6cfb98270a1b23d41
Mongoose10 <nishanthshiroor10@gmail.com>
2024-07-14 16:47:45 +0530

    Organised the files into a folder

---
+++ PT-VP Comparison/Comparison btw VP and PT.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 10:58:03 2024
+
+@author: 91959
+"""
+
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+A = np.linspace(0.01,0.5,30)
+
+vp = []
+
+pt = []
+
+pot = []
+
+qm = []
+
+with open('VP energy.csv', 'r') as csvfile:
+    lines = csv.reader(csvfile,delimiter=',')
+    next(lines)
+    for row in lines:
+        vp.append(float(row[1]))
+     
+with open('PT energy.csv', 'r') as csvfile:
+    lines = csv.reader(csvfile,delimiter=',')
+    next(lines)
+    for row in lines:
+        pt.append(float(row[1]))
+        pot.append(float(row[2]))
+        qm.append(float(row[3]))
+    
+vp = np.array(vp)
+pot = np.array(pot)
+qm = np.array(qm)
+
+actualpt = pt + pot
+
+PT = qm - pt
+
+VP = qm - vp
+
+print(pt)
+print()
+print(vp)
+print()
+print(qm)
+
+plt.plot(A,PT,label='Perturbation Theory')
+plt.plot(A,VP,'g',label='Variational Principle')
+plt.xlabel('A')
+plt.ylabel('Deviation from quantum solver value')
+plt.title('Comparison btw Perturbation Theory and Variational Principle')
+plt.legend(loc='best')
+
+#plt.xlim(0.005,0.065)
---
+++ PT-VP Comparison/Quantum Solver.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 10:20:45 2024
+
+@author: 91959
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+omega = 1.00
+An = np.linspace(0.01,0.5,30)
+#A = 3
+sigma = 0.25
+vmin = []
+pt = []
+
+def V(x):
+    return 0.5*omega*omega*x*x + A*np.exp(-(x**2)/(2*sigma**2))
+
+ngrid = 2048
+xmax=10.0
+xmin=-10.0
+qm = []
+
+dx= (xmax-xmin)/ngrid
+H=np.zeros((ngrid,ngrid))
+pot = np.zeros(ngrid)
+onbydx2=1.0/(dx*dx)
+
+for j in range(30): 
+    A = An[j]
+    
+    for i in range(ngrid):
+
+        if i!=0 and i!=ngrid-1:
+            H[i-1][i]=-0.5*onbydx2
+            H[i+1][i]=-0.5*onbydx2
+        elif i==0:
+                H[i+1][i]=-0.5*onbydx2
+        elif i==ngrid-1:
+                H[i-1][i]=-0.5*onbydx2
+    
+        xval=xmin+i*dx
+        pot[i] = V(xval)
+        H[i][i]=onbydx2 + V(xval)
+
+    w,v=np.linalg.eigh(H)
+        
+    vmin.append(np.min(pot))    
+    pt.append(0.5 + A*np.sqrt((2*sigma**2)/(1 + 2*sigma**2)))
+    qm.append(w[0])
+    
+#print(np.min(pot))
+
+dict = {'Perturbation Theory Energy':pt,'Potential':vmin,'Quantum Solver Energy':qm}
+df = pd.DataFrame(dict)
+     
+df.to_csv('PT energy.csv')
+
+
+#print(w[0])
+#v2 = v.transpose()
+
+#plt.plot(v2[0],'black')
+#plt.plot(pot)
+#plt.ylim(bottom = -1.0, top=2.0)
+
+
